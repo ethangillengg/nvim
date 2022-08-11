f7ff9a00372331f67f1476c10b77f7a2d962ace5
ethangillengg <ethan.gill@ucalgary.ca>
2022-08-11 15:37:14 -0600

    delete everything

--- lua/swarm/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if(not status_ok) then
-  return
-end
-
-require("swarm.lsp.lsp-installer")
-require("swarm.lsp.handlers").setup()
-require("swarm.lsp.null-ls")
