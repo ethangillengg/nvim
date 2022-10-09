4c02fa77503e983d7bc0bb6e067b739beb488186
ethangillengg <ethan.gill@ucalgary.ca>
2022-10-08 23:09:56 -0600

    feat: add keymap for horizontal split

--- lua/ethan/keymaps.lua
+++ lua/ethan/keymaps.lua
@@ -1,70 +1,62 @@
-local opts = { noremap = true, silent = true }
 local map = function(mode, key, command)
-  vim.api.nvim_set_keymap(
-    mode,
-    key,
-    command,
-    { noremap = true, silent = true }
-  )
+	vim.api.nvim_set_keymap(mode, key, command, { noremap = true, silent = true })
 end
 
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 -- Quick nav splits
-map('n', '<c-h>', '<c-w>h')
-map('n', '<c-l>', '<c-w>l')
-map('n', '<c-j>', '<c-w>j')
-map('n', '<c-k>', '<c-w>k')
+map("n", "<c-w>h", ":split<cr>")
+map("n", "<c-w><c-h>", ":split<cr>")
+map("n", "<c-h>", "<c-w>h")
+map("n", "<c-l>", "<c-w>l")
+map("n", "<c-j>", "<c-w>j")
+map("n", "<c-k>", "<c-w>k")
 
-map('n', '<leader>w', ':wa<cr>')
-map('n', '<leader>q', ':wqa<cr>')
+map("n", "<leader>w", ":wa<cr>")
+map("n", "<leader>q", ":wqa<cr>")
 map("n", "<Leader>v", ":edit ~/.config/nvim/lua/init.lua<CR>")
 
 -- Navigate buffers
-map('n', '<S-l>', ':bnext<cr>')
-map('n', '<S-h>', ':bprevious<cr>')
+map("n", "<S-l>", ":bnext<cr>")
+map("n", "<S-h>", ":bprevious<cr>")
 
 -- Dont stop visual mode on indent
-map('v', '<', '<gv')
-map('v', '>', '>gv')
-
+map("v", "<", "<gv")
+map("v", ">", ">gv")
 
 -- Move text with alt-j/k
-map('n', '<A-j>', ':m .+1<CR>==')
-map('n', '<A-k>', ':m .-2<CR>==')
-map('i', '<A-j>', '<Esc>:m .+1<CR>==gi')
-map('i', '<A-k>', '<Esc>:m .-2<CR>==gi')
-map('v', '<A-j>', ":m '>+1<CR>gv-gv")
-map('v', '<A-k>', ":m '<-2<CR>gv-gv")
+map("n", "<A-j>", ":m .+1<CR>==")
+map("n", "<A-k>", ":m .-2<CR>==")
+map("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
+map("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
+map("v", "<A-j>", ":m '>+1<CR>gv-gv")
+map("v", "<A-k>", ":m '<-2<CR>gv-gv")
 
 -- Navigate buffers
 map("n", "<S-l>", ":bnext<CR>")
 map("n", "<S-h>", ":bprevious<CR>")
 
 -- Don't overrite register on paste in visual mode
-map('v', 'p', '"_dP')
-
+map("v", "p", '"_dP')
 
 -- Telescope mappings
 local telescope, _ = pcall(require, "telescope")
 if telescope then
-  map('n', '<leader>c', ':Telescope colorscheme<CR>')
-  map('n', '<leader>r', ':Telescope live_grep<CR>')
-  map('n', '<leader>h', ':Telescope help_tags<CR>')
-  map('n', '<leader>l', ':Telescope colorscheme<CR>')
-  map('n', '<c-p>', ':Telescope find_files<CR>')
+	map("n", "<leader>c", ":Telescope colorscheme<CR>")
+	map("n", "<leader>r", ":Telescope live_grep<CR>")
+	map("n", "<leader>h", ":Telescope help_tags<CR>")
+	map("n", "<leader>l", ":Telescope colorscheme<CR>")
+	map("n", "<c-p>", ":Telescope find_files<CR>")
 end
 
 -- Neotree mappings
 local neotree, _ = pcall(require, "neo-tree")
 if neotree then
-  map('n', '<leader>e', ':NeoTreeFloatToggle<cr>')
+	map("n", "<leader>e", ":NeoTreeFloatToggle<cr>")
 end
-
 
 -- Mason mappings
 local mason, _ = pcall(require, "mason")
 if mason then
-  map('n', '<leader>m', ':Mason<cr>')
+	map("n", "<leader>m", ":Mason<cr>")
 end
-
