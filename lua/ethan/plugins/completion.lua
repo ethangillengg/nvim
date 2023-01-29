e70cccb5f0ab8e68eef4c54bdaa94bb6b700e5fd
ethangillengg <ethan.gill@ucalgary.ca>
2023-01-28 21:31:59 -0700

    refactor: remove snippets + floaterm

--- lua/ethan/plugins/completion.lua
+++ lua/ethan/plugins/completion.lua
@@ -1,45 +1,45 @@
 --cmp in commandline!
 local cmp_cmdline = {
-  "hrsh7th/cmp-cmdline",
-  event = "CmdlineEnter",
-  config = function()
-    local cmp = require("cmp")
-    cmp.setup.cmdline("/", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" },
-      },
-    })
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        {
-          name = "cmdline",
-          option = {
-            ignore_cmds = { "Man", "!" },
-          },
-        },
-      }),
-    })
-  end,
+	"hrsh7th/cmp-cmdline",
+	event = "CmdlineEnter",
+	config = function()
+		local cmp = require("cmp")
+		cmp.setup.cmdline("/", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{
+					name = "cmdline",
+					option = {
+						ignore_cmds = { "Man", "!" },
+					},
+				},
+			}),
+		})
+	end,
 }
 
 return {
-  -- snippets
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
-      end,
-    },
-    opts = {
-      history = true,
-      delete_check_events = "TextChanged",
-    },
+	-- snippets
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
+		},
+		opts = {
+			history = true,
+			delete_check_events = "TextChanged",
+		},
     -- stylua: ignore
     keys = {
       {
@@ -52,94 +52,94 @@ return {
       { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
       { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
     },
-  },
+	},
 
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-nvim-lua",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-emoji",
-      cmp_cmdline,
-    },
-    opts = function()
-      local cmp = require("cmp")
-      return {
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete({ reason = "manual" }),
-          ["<C-j>"] = cmp.mapping.scroll_docs(4),
-          ["<C-k>"] = cmp.mapping.scroll_docs(-4),
-          ["<Tab>"] = cmp.mapping.select_next_item(),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        }),
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			-- "saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-emoji",
+			cmp_cmdline,
+		},
+		opts = function()
+			local cmp = require("cmp")
+			return {
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
+				-- snippet = {
+				-- 	expand = function(args)
+				-- 		require("luasnip").lsp_expand(args.body)
+				-- 	end,
+				-- },
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete({ reason = "manual" }),
+					["<C-j>"] = cmp.mapping.scroll_docs(4),
+					["<C-k>"] = cmp.mapping.scroll_docs(-4),
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<S-Tab>"] = cmp.mapping.select_prev_item(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
 
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "nvim_lua" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
-          { name = "emoji" },
-        }),
-        formatting = {
-          fields = { "kind", "abbr", "menu" },
-          format = function(entry, vim_item)
-            local icons = require("ethan.icons")
-            -- Kind icons
-            vim_item.kind = icons.kind[vim_item.kind]
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "nvim_lua" },
+					-- { name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "path" },
+					{ name = "emoji" },
+				}),
+				formatting = {
+					fields = { "kind", "abbr", "menu" },
+					format = function(entry, vim_item)
+						local icons = require("ethan.icons")
+						-- Kind icons
+						vim_item.kind = icons.kind[vim_item.kind]
 
-            -- NOTE: order matters
-            vim_item.menu = ({
-              nvim_lsp = "[LSP]",
-              nvim_lua = "[LUA]",
-              luasnip = "[Snippet]",
-              buffer = "[Buffer]",
-              path = "[Path]",
-              emoji = "[Emoji]",
-            })[entry.source.name]
-            return vim_item
-          end,
-        },
-        window = {
-          documentation = {
-            border = "rounded",
-            winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None",
-            zindex = 1001,
-            col_offset = 0,
-            side_padding = 1,
-          },
-          completion = {
-            border = "rounded",
-            winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None",
-            zindex = 1001,
-            col_offset = 0,
-            side_padding = 1,
-          },
-        },
-        experimental = {
-          ghost_text = {
-            hl_group = "LspCodeLens",
-          },
-        },
-      }
-    end,
-    config = function(_, opts)
-      vim.g.cmp_active = true
-      require("cmp").setup(opts)
-    end,
-  },
+						-- NOTE: order matters
+						vim_item.menu = ({
+							nvim_lsp = "[LSP]",
+							nvim_lua = "[LUA]",
+							-- luasnip = "[Snippet]",
+							buffer = "[Buffer]",
+							path = "[Path]",
+							emoji = "[Emoji]",
+						})[entry.source.name]
+						return vim_item
+					end,
+				},
+				window = {
+					documentation = {
+						border = "rounded",
+						winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None",
+						zindex = 1001,
+						col_offset = 0,
+						side_padding = 1,
+					},
+					completion = {
+						border = "rounded",
+						winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None",
+						zindex = 1001,
+						col_offset = 0,
+						side_padding = 1,
+					},
+				},
+				experimental = {
+					ghost_text = {
+						hl_group = "LspCodeLens",
+					},
+				},
+			}
+		end,
+		config = function(_, opts)
+			vim.g.cmp_active = true
+			require("cmp").setup(opts)
+		end,
+	},
 }
--- lua/ethan/plugins/core.lua
+++ lua/ethan/plugins/core.lua
@@ -1,115 +1,111 @@
 return {
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }, --treesitter
-	{ "nvim-lua/popup.nvim" }, --other api for stuffs
-	{ "nvim-lua/plenary.nvim" }, --api for stuffs
-	{ "kyazdani42/nvim-web-devicons" }, --icons for stuffs
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }, --treesitter
+  { "nvim-lua/popup.nvim" }, --other api for stuffs
+  { "nvim-lua/plenary.nvim" }, --api for stuffs
+  { "kyazdani42/nvim-web-devicons" }, --icons for stuffs
 
-	{ "windwp/nvim-autopairs", config = true }, --bracket pairs
-	{ "windwp/nvim-ts-autotag", config = true }, -- other auto pairs (html for example)
+  { "windwp/nvim-autopairs", config = true }, --bracket pairs
+  { "windwp/nvim-ts-autotag", config = true }, -- other auto pairs (html for example)
 
-	{
-		"nvim-telescope/telescope.nvim",
-		opts = {
-			defaults = {
-				prompt_prefix = "   ",
-				selection_caret = "❯ ",
-				sorting_strategy = "ascending",
-				layout_config = {
-					prompt_position = "top",
-				},
-			},
-		},
-	}, --telescope
-	--[[ { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- fast sorter for telescope ]]
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        prompt_prefix = "   ",
+        selection_caret = "❯ ",
+        sorting_strategy = "ascending",
+        layout_config = {
+          prompt_position = "top",
+        },
+      },
+    },
+  }, --telescope
+  --[[ { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- fast sorter for telescope ]]
 
-	{
-		"numToStr/Comment.nvim",
-		--configure later?
-		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-		config = true,
-	}, -- Easily comment stuff
+  {
+    "numToStr/Comment.nvim",
+    --configure later?
+    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+    config = true,
+  }, -- Easily comment stuff
 
-	{
-		"voldikss/vim-floaterm",
-		config = function()
-			vim.g.floaterm_height = 0.99
-			vim.g.floaterm_width = 0.99
-		end,
-	},
+  -- git integration
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        change = {
+          hl = "GitSignsChange",
+          text = "▎",
+          numhl = "GitSignsChangeNr",
+          linehl = "GitSignsChangeLn",
+        },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = {
+          hl = "GitSignsChange",
+          text = "▎",
+          numhl = "GitSignsChangeNr",
+          linehl = "GitSignsChangeLn",
+        },
+        untracked = { text = "▎" },
+        --[[ untracked = { text = "┆" }, ]]
+      },
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        delay = 0,
+        virt_text_priority = 0,
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+      },
+    },
+  },
 
-	-- git integration
-	{
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-				change = {
-					hl = "GitSignsChange",
-					text = "▎",
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
-				},
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = {
-					hl = "GitSignsChange",
-					text = "▎",
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
-				},
-				untracked = { text = "▎" },
-				--[[ untracked = { text = "┆" }, ]]
-			},
-			current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-			current_line_blame_opts = {
-				delay = 0,
-				virt_text_priority = 0,
-				virt_text = true,
-				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-			},
-		},
-	},
+  -- keymaps
+  {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 500
+      require("which-key").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  },
 
-	-- keymaps
-	{
-		"folke/which-key.nvim",
-		config = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 500
-			require("which-key").setup({
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			})
-		end,
-	},
+  -- UI
+  { "stevearc/dressing.nvim" }, -- styling for lsp rename and code actions
 
-	-- UI
-	{ "stevearc/dressing.nvim" }, -- styling for lsp rename and code actions
-
-	-- Lets you use git and file managers
-	{
-		"is0n/fm-nvim",
-		keys = {
-			-- Lf + lazygit mappings (fm-nvim)
-			-- map("n", "<leader>d", ":Gitui<cr>")
-			{ "<leader>d", "<cmd>Gitui<cr>", desc = "Open gitui" },
-			{
-				"<leader>e",
-				function()
-					-- command to start lf at the current file
-					require("fm-nvim").Lf(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()))
-				end,
-				desc = "Lf at current file",
-			},
-		},
-		opts = {
-			ui = {
-				float = {
-					height = 1,
-					width = 0.9,
-				},
-			},
-		},
-	},
+  -- Lets you use git and file managers
+  {
+    "is0n/fm-nvim",
+    keys = {
+      -- Lf + lazygit mappings (fm-nvim)
+      -- map("n", "<leader>d", ":Gitui<cr>")
+      { "<leader>d", "<cmd>Gitui<cr>", desc = "Open gitui" },
+      {
+        "<leader>e",
+        function()
+          -- command to start lf at the current file
+          require("fm-nvim").Lf(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()))
+        end,
+        desc = "Lf at current file",
+      },
+    },
+    opts = {
+      mappings = {
+        ESC = "q",
+      },
+      ui = {
+        float = {
+          border = "rounded",
+          height = 1,
+          width = 0.9,
+        },
+      },
+    },
+  },
 }
