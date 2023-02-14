e85a1442b49a3eef6ce43c029847d468025286d5
Ethan Gill <EGill@sunwaptasolutions.com>
2023-02-14 12:41:59 -0700

    feat: cmp transparency

--- lua/ethan/plugins/colorschemes.lua
+++ lua/ethan/plugins/colorschemes.lua
@@ -1,28 +1,31 @@
 return {
-	-- colors!!
-	{ "catppuccin/nvim", name = "catppuccin" },
-	{ "kyazdani42/blue-moon" }, --cool colorscheme
-	{ "sainnhe/sonokai" },
-	{ "folke/tokyonight.nvim" },
+    -- colors!!
+    { "catppuccin/nvim",         name = "catppuccin" },
+    { "kyazdani42/blue-moon" }, --cool colorscheme
+    { "sainnhe/sonokai" },
+    { "folke/tokyonight.nvim" },
+    { "ellisonleao/gruvbox.nvim" },
 
-	--allow transparency for colorschemes that don't support it
-	{
-		"xiyaowong/nvim-transparent",
-		enable = true,
-		opts = {
-			enable = true,
-			extra_groups = { -- table/string: additional groups that should be cleared
-				-- In particular, when you set it to 'all', that means all available groups
-
-				-- example of akinsho/nvim-bufferline.lua
-				-- "BufferLineTabClose",
-				-- "BufferlineBufferSelected",
-				-- "BufferLineFill",
-				-- "BufferLineBackground",
-				-- "BufferLineSeparator",
-				-- "BufferLineIndicatorSelected",
-			},
-			exclude = {}, -- table: groups you don't want to clear
-		},
-	},
+    --allow transparency for colorschemes that don't support it
+    {
+        "xiyaowong/nvim-transparent",
+        enable = true,
+        opts = {
+            enable = true,
+            extra_groups = { -- table/string: additional groups that should be cleared
+                -- In particular, when you set it to 'all', that means all available groups
+                "all",
+                "NormalFloat",
+                "FloatBorder",
+                -- example of akinsho/nvim-bufferline.lua
+                -- "BufferLineTabClose",
+                -- "BufferlineBufferSelected",
+                -- "BufferLineFill",
+                -- "BufferLineBackground",
+                -- "BufferLineSeparator",
+                -- "BufferLineIndicatorSelected",
+            },
+            exclude = {}, -- table: groups you don't want to clear
+        },
+    },
 }
