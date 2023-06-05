6ae5f03dbbb73ccc66fdae73caf5afa0c3eb044f
Ethan Gill <EGill@sunwaptasolutions.com>
2023-06-05 10:46:40 -0600

    feat: add html autotag

--- plugins.lua
+++ plugins.lua
@@ -1,4 +1,4 @@
-local overrides = require("custom.configs.overrides")
+local overrides = require "custom.configs.overrides"
 
 ---@type NvPluginSpec[]
 local plugins = {
@@ -25,7 +25,7 @@ local plugins = {
   -- override plugin configs
   {
     "williamboman/mason.nvim",
-    opts = overrides.mason
+    opts = overrides.mason,
   },
 
   {
@@ -46,6 +46,7 @@ local plugins = {
       require("better_escape").setup()
     end,
   },
+  { "windwp/nvim-ts-autotag", event = "BufEnter", config = true },
 
   -- To make a plugin not be loaded
   -- {
