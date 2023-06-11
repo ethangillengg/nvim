58905681835c26bb617b110b492ba456955ea92f
Ethan Gill <ethan.gill@ucalgary.ca>
2023-06-10 20:40:28 -0600

    fix: nix lsp name

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -5,7 +5,7 @@ local rt = require "rust-tools"
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "volar", "csharp_ls", "rust_analyzer", "nil" }
+local servers = { "html", "cssls", "tsserver", "clangd", "volar", "csharp_ls", "rust_analyzer", "nil_ls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
