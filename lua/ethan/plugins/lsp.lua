877b48434db36ba1aa3d8af750a0c81f8fdb50d6
Ethan Gill <ethan.gill@ucalgary.ca>
2023-09-28 13:10:15 -0600

    feat: limit hover window width

--- lua/ethan/plugins/lsp.lua
+++ lua/ethan/plugins/lsp.lua
@@ -40,9 +40,8 @@ return {
 
 			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 				border = "rounded",
-			})
-			vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-				border = "rounded",
+				width = 100,
+				title = "Hover",
 			})
 
 			require("mason").setup(opts)
