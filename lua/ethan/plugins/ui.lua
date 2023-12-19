d7335c81e6c5331e30c07d9bc5e521086e48ec67
Ethan Gill <EGill@sunwaptasolutions.com>
2023-12-19 10:31:26 -0700

    feat: alpha windows notes

--- lua/ethan/plugins/ui.lua
+++ lua/ethan/plugins/ui.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"goolord/alpha-nvim",
+		event = "VimEnter",
 		opts = function()
 			local dashboard = require("alpha.themes.dashboard")
 			local function button(sc, txt, keybind, keybind_opts)
@@ -12,46 +13,97 @@ return {
 			local icons = require("ethan.icons")
 
 			dashboard.section.header.val = {
-				[[                               __                ]],
-				[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-				[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-				[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-				[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-				[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+				[[                                                    ]],
+				[[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
+				[[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
+				[[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
+				[[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+				[[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+				[[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
 			}
+			local function test()
+				vim.cmd(":echo 'test'")
+			end
+
 			dashboard.section.buttons.val = {
 				button("p", icons.documents.Files .. " Find file", ":Telescope find_files <CR>"),
 				button("e", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
-				button("n", icons.ui.Note .. " Notes", ":edit ~/Notes/index.md<CR>"),
 				button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
 				button("t", icons.ui.Abc .. " Find text", ":Telescope live_grep <CR>"),
 				-- button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
+				{
+					type = "button",
+					val = icons.ui.Note .. " Notes",
+					on_press = function()
+						local key = vim.api.nvim_replace_termcodes("n" .. "<Ignore>", true, false, true)
+						vim.api.nvim_feedkeys(key, "t", false)
+					end,
+					opts = {
+						position = "center",
+						shortcut = ("n"):gsub("%s", ""):gsub("SPC", "<leader>"),
+						cursor = 3,
+						width = 50,
+						align_shortcut = "right",
+						hl_shortcut = "Macro",
+
+						keymap = {
+							"n",
+							"n",
+							"",
+							{
+								callback = function()
+									local this_os = vim.loop.os_uname().sysname
+									if this_os == "Linux" then
+										vim.cmd(":e ~/Notes/index.md")
+									else
+										vim.cmd(":e C:\\Users\\EGill\\Notes\\index.md")
+									end
+								end,
+								noremap = true,
+								silent = true,
+								nowait = true,
+							},
+						},
+					},
+				},
 				button("l", icons.misc.Package .. " Plugins", ":Lazy<CR>"),
 				button("u", icons.ui.CloudDownload .. " Update", ":Lazy sync<CR>"),
 				button("q", icons.ui.SignOut .. " Quit", ":qa<CR>"),
 			}
-			local function footer()
-				-- NOTE: requires the fortune-mod package to work
-				-- local handle = io.popen("fortune")
-				-- local fortune = handle:read("*a")
-				-- handle:close()
-				-- return fortune
-				return "ethan.gill@ucalgary.ca"
-			end
 
-			dashboard.section.footer.val = footer()
+			-- Print Lazy stats in footer
+			vim.api.nvim_create_autocmd("User", {
+				once = true,
+				pattern = "LazyVimStarted",
+				callback = function()
+					local stats = require("lazy").stats()
+					local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+
+					dashboard.section.footer.val = " Neovim loaded "
+						.. stats.loaded
+						.. "/"
+						.. stats.count
+						.. " plugins in "
+						.. ms
+						.. "ms"
+						.. " 🚀"
+					pcall(vim.cmd.AlphaRedraw)
+				end,
+			})
 
 			dashboard.section.header.opts.hl = "Include"
 			dashboard.section.buttons.opts.hl = "Macro"
 			dashboard.section.footer.opts.hl = "Type"
 
 			dashboard.opts.opts.noautocmd = true
+
 			return dashboard
 		end,
 		config = function(_, dashboard)
 			-- hide the statusline
 			vim.opt_global.laststatus = 0
 			require("alpha").setup(dashboard.opts)
+			-- require("alpha").setup()
 		end,
 	}, --greeter
 	{
