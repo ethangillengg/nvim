ff5cf7253bb39f16a03883e79d07f4ee7479ec17
Ethan Gill <EGill@sunwaptasolutions.com>
2023-12-20 11:28:12 -0700

    feat: mkdnflow mappings

--- lua/ethan/plugins/markdown.lua
+++ lua/ethan/plugins/markdown.lua
@@ -1,52 +1,63 @@
 return {
-	-- {
-	-- 	"epwalsh/obsidian.nvim",
-	-- 	lazy = true,
-	-- 	event = {
-	-- 		"BufReadPre /home/ethan/Notes/Obsidian/**.md",
-	-- 		"BufNewFile /home/ethan/Notes/Obsidian/**.md",
-	-- 	},
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 	},
-	--
-	-- 	cmd = { "ObsidianQuickSwitch" },
-	-- 	keys = {
-	-- 		-- { "gd", "<cmd>ObsidianFollowLink<cr>", desc = "Follow Obsidian Link" },
-	-- 		{ "<enter>", "<cmd>ObsidianFollowLink<cr>", desc = "Follow Obsidian Link" },
-	-- 		{ "<leader>to", "<cmd>ObsidianQuickSwitch<cr>", desc = "Find Note" },
-	-- 	},
-	-- 	opts = {
-	-- 		-- Obsidian automatically uses the first workspace as default
-	-- 		workspaces = {
-	-- 			{
-	-- 				name = "personal",
-	-- 				path = "~/Notes/Obsidian/Personal",
-	-- 			},
-	-- 			{
-	-- 				name = "work",
-	-- 				path = "~/Notes/Obsidian/Work",
-	-- 			},
-	-- 		},
-	-- 		completion = {
-	-- 			-- Start cmp immediately
-	-- 			min_chars = 1,
-	-- 		},
-	--
-	-- 		follow_url_func = function(url)
-	-- 			-- Open the URL in the default web browser.
-	-- 			vim.fn.jobstart({ "xdg-open", url })
-	-- 		end,
-	-- 	},
-	-- },
-
 	{
 		"jakewvincent/mkdnflow.nvim",
-		config = function()
-			require("mkdnflow").setup({
-				-- Config goes here; leave blank for defaults
-			})
-		end,
+		opts = {
+			to_do = {
+				symbols = { " ", "~", "X" },
+			},
+
+			mappings = {
+				MkdnEnter = false,
+				MkdnTab = false,
+				MkdnSTab = false,
+				MkdnNextLink = false,
+				MkdnPrevLink = false,
+				MkdnNextHeading = { "n", "]]" },
+				MkdnPrevHeading = { "n", "[[" },
+				MkdnGoBack = false,
+				MkdnGoForward = false,
+				MkdnCreateLink = false, -- see MkdnEnter
+				MkdnCreateLinkFromClipboard = { { "n", "v" }, "<leader>p" }, -- see MkdnEnter
+				MkdnFollowLink = false, -- see MkdnEnter
+				MkdnDestroyLink = { "n", "<M-CR>" },
+				MkdnTagSpan = { "v", "<M-CR>" },
+				MkdnMoveSource = { "n", "<F2>" },
+				MkdnYankAnchorLink = { "n", "yaa" },
+				MkdnYankFileAnchorLink = { "n", "yfa" },
+				MkdnIncreaseHeading = { "n", "+" },
+				MkdnDecreaseHeading = { "n", "-" },
+				MkdnToggleToDo = { { "n", "v" }, "<C-]>" },
+				MkdnNewListItem = { "i", "<CR>" }, -- seee MkdnEnter
+				MkdnNewListItemBelowInsert = { "n", "o" },
+				MkdnNewListItemAboveInsert = { "n", "O" },
+				MkdnExtendList = false,
+				MkdnUpdateNumbering = { "n", "<leader>nn" },
+				MkdnTableNextCell = { "i", "<Tab>" },
+				MkdnTablePrevCell = { "i", "<S-Tab>" },
+				MkdnTableNextRow = false,
+				MkdnTablePrevRow = { "i", "<M-CR>" },
+				MkdnTableNewRowBelow = { "n", "<leader>ir" },
+				MkdnTableNewRowAbove = { "n", "<leader>iR" },
+				MkdnTableNewColAfter = { "n", "<leader>ic" },
+				MkdnTableNewColBefore = { "n", "<leader>iC" },
+				MkdnFoldSection = false, -- use default fold binds
+				MkdnUnfoldSection = false, -- use default fold binds
+			},
+		},
+
+		keys = {
+			{
+				"<CR>",
+				function()
+					local flow = require("mkdnflow")
+					vim.cmd([[normal! m`]]) -- push to jumplist
+					flow.cursor.toNextLink()
+					flow.links.followLink()
+				end,
+				mode = "n",
+				desc = "Follow Link",
+			},
+		},
 	},
 	{ "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
 	{
