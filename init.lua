80d98a99e88ecc6ddb196d6cd4e4698e83035a2b
Ethan Gill <EGill@sunwaptasolutions.com>
2024-02-19 13:20:41 -0700

    feat(themes): gruvbox material (treesitter support)

--- init.lua
+++ init.lua
@@ -8,4 +8,4 @@ profile_require("options")
 profile_require("mappings")
 profile_require("setup-plugins")
 
-vim.cmd("colorscheme tokyonight-night")
+vim.cmd("colorscheme gruvbox-material")
--- lua/ethan/plugins/colorschemes.lua
+++ lua/ethan/plugins/colorschemes.lua
@@ -4,6 +4,13 @@ return {
 	{ "folke/tokyonight.nvim" },
 	{ "ellisonleao/gruvbox.nvim" },
 	{
+		"sainnhe/gruvbox-material",
+		config = function()
+			vim.g.gruvbox_material_background = "medium"
+			vim.g.gruvbox_material_foreground = "original"
+		end,
+	},
+	{
 		"marko-cerovac/material.nvim",
 		config = function()
 			vim.g.material_style = "darker"
