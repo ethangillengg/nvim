f22bd57b6f8fd8183ec7555ff8c24b2bb1d2524d
Ethan Gill <ethan.gill@ucalgary.ca>
2024-06-09 22:57:15 -0600

    feat: neo-tree reveal

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -2,15 +2,15 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '<c-n>', ':Neotree toggle<CR>', { desc = 'NeoTree toggle' } },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "<c-n>", ":Neotree toggle reveal<CR>", { desc = "NeoTree toggle" } },
+	},
 }
