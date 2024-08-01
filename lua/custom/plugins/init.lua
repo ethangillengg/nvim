324c0eecd7698e6f38f12660d8bb9ac8c78d7af4
Ethan Gill <ethan.gill@ucalgary.ca>
2024-08-01 10:06:11 -0600

    feat: git conflict plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,45 +3,45 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'akinsho/git-conflict.nvim',
-    version = '*',
-    event = 'BufReadPost',
-    init = function()
-      vim.api.nvim_create_autocmd('BufReadPre', { command = 'GitConflictRefresh' })
-    end,
-    opts = {
-      list_opener = 'Telescope quickfix',
-      default_mappings = {
-        ours = 'co',
-        theirs = 'ct',
-        none = 'c0',
-        both = 'cb',
-        next = 'cn',
-        prev = 'cN',
-      },
-      highlights = {
-        incoming = 'DiffAdd',
-        current = 'DiffChange',
-      },
-    },
-    cmd = {
-      'GitConflictChooseBase',
-      'GitConflictChooseTheirs',
-      'GitConflictChooseOurs',
-      'GitConflictChooseBoth',
-      'GitConflictChooseNone',
-      'GitConflictIncoming',
-      'GitConflictIncomingLabel',
-      'GitConflictListQf',
-      'GitConflictNextConflict',
-      'GitConflictPrevConflict',
-      'GitConflictRefresh',
-    },
+	{
+		"akinsho/git-conflict.nvim",
+		version = "*",
+		event = "BufReadPost",
+		init = function()
+			vim.api.nvim_create_autocmd("BufReadPre", { command = "GitConflictRefresh" })
+		end,
+		opts = {
+			list_opener = "Telescope quickfix",
+			default_mappings = {
+				ours = "co",
+				theirs = "ct",
+				none = "c0",
+				both = "cb",
+				next = "cn",
+				prev = "cN",
+			},
+			highlights = {
+				incoming = "DiffAdd",
+				current = "DiffChange",
+			},
+		},
+		cmd = {
+			"GitConflictChooseBase",
+			"GitConflictChooseTheirs",
+			"GitConflictChooseOurs",
+			"GitConflictChooseBoth",
+			"GitConflictChooseNone",
+			"GitConflictIncoming",
+			"GitConflictIncomingLabel",
+			"GitConflictListQf",
+			"GitConflictNextConflict",
+			"GitConflictPrevConflict",
+			"GitConflictRefresh",
+		},
 
-    config = true,
-    keys = {
-      { '<leader>gq', '<cmd>GitConflictListQf<CR>', desc = 'Git: Conflicts' },
-    },
-  },
+		config = true,
+		keys = {
+			{ "<leader>gq", "<cmd>GitConflictListQf<CR>", desc = "[G]it Conflict [Q]uickfix" },
+		},
+	},
 }
