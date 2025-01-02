a1489d4f8c65dbacc92f9aab8020a14b688e25d1
Ethan Gill <gill.ethan123@gmail.com>
2025-01-01 23:40:42 -0700

    feat: custom dap ui

--- lua/kickstart/plugins/dap.lua
+++ lua/kickstart/plugins/dap.lua
@@ -11,7 +11,48 @@ return {
 			local dap = require("dap")
 			local ui = require("dapui")
 
-			require("dapui").setup()
+			require("dapui").setup({
+				controls = {
+					element = "console",
+				},
+				layouts = {
+					{
+						elements = {
+							{
+								id = "scopes",
+								size = 0.4,
+							},
+							{
+								id = "breakpoints",
+								size = 0.3,
+							},
+							{
+								id = "stacks",
+								size = 0.3,
+							},
+							-- {
+							-- 	id = "watches",
+							-- 	size = 0.25,
+							-- },
+						},
+						position = "left",
+						size = 40,
+					},
+					{
+						elements = {
+							{
+								id = "console",
+							},
+							-- {
+							-- 	id = "console",
+							-- 	size = 0.5,
+							-- },
+						},
+						position = "top",
+						size = 1,
+					},
+				},
+			})
 
 			require("nvim-dap-virtual-text").setup({
 				-- -- This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
