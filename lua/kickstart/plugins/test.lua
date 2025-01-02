325eb15243c58888bb6ed7847e816cc6d16e5727
Ethan Gill <gill.ethan123@gmail.com>
2025-01-02 12:39:20 -0700

    feat: neotest braille loader

--- lua/kickstart/plugins/test.lua
+++ lua/kickstart/plugins/test.lua
@@ -10,6 +10,9 @@ return {
 		},
 		config = function()
 			require("neotest").setup({
+				icons = {
+					running_animated = { "⠻", "⠽", "⠾", "⠷", "⠯", "⠟" },
+				},
 				adapters = {
 					require("neotest-dotnet"),
 				},
