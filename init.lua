b6b3076e185b24f47ee4534be8f913df1f697175
Ethan Gill <gill.ethan123@gmail.com>
2025-01-09 13:22:35 -0700

    fix: cs decompilation support

--- init.lua
+++ init.lua
@@ -317,7 +317,7 @@ require("lazy").setup({
 						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 					end
 
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
 					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
