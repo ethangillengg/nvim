1443a8bf51ba103873e43780fbd024931e97f323
Ethan Gill <gill.ethan123@gmail.com>
2025-02-28 08:17:25 -0700

    feat(csharp): razor support (commented out)

--- lua/kickstart/plugins/csharp.lua
+++ lua/kickstart/plugins/csharp.lua
@@ -1,6 +1,6 @@
 return {
 	{
-		"seblj/roslyn.nvim",
+		"seblyng/roslyn.nvim",
 		ft = { "cs" },
 		opts = {
 			exe = "Microsoft.CodeAnalysis.LanguageServer",
@@ -8,25 +8,79 @@ return {
 		},
 	},
 	-- {
-	-- 	"iabdelkareem/csharp.nvim",
-	-- 	lazy = false,
+	-- 	"seblyng/roslyn.nvim",
+	-- 	ft = { "cs", "razor" },
 	-- 	dependencies = {
-	-- 		"mfussenegger/nvim-dap",
-	-- 		"Tastyep/structlog.nvim", -- Optional, but highly recommended for debugging
+	-- 		{
+	-- 			-- By loading as a dependencies, we ensure that we are available to set
+	-- 			-- the handlers for roslyn
+	-- 			"tris203/rzls.nvim",
+	-- 			config = function()
+	-- 				---@diagnostic disable-next-line: missing-fields
+	-- 				require("rzls").setup({
+	-- 					path = "/nix/store/r9547xfjfvwpwiq7ydf4pb1xihx72ms3-user-environment/bin/rzls",
+	-- 				})
+	-- 			end,
+	-- 		},
 	-- 	},
 	-- 	config = function()
-	-- 		require("csharp").setup({
-	-- 			lsp = {
-	-- 				omnisharp = {
-	-- 					enable = false,
-	-- 					-- cmd_path = "OmniSharp",
+	-- 		require("roslyn").setup({
+	-- 			exe = "Microsoft.CodeAnalysis.LanguageServer",
+	-- 			filewatching = false,
+	-- 			args = {
+	-- 				"--stdio",
+	-- 				"--logLevel=Information",
+	-- 				"--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+	-- 				"--razorSourceGenerator=" .. vim.fs.joinpath(
+	-- 					vim.fn.stdpath("data") --[[@as string]],
+	-- 					"mason",
+	-- 					"packages",
+	-- 					"roslyn",
+	-- 					"libexec",
+	-- 					"Microsoft.CodeAnalysis.Razor.Compiler.dll"
+	-- 				),
+	-- 				"--razorDesignTimePath=" .. vim.fs.joinpath(
+	-- 					vim.fn.stdpath("data") --[[@as string]],
+	-- 					"mason",
+	-- 					"packages",
+	-- 					"rzls",
+	-- 					"libexec",
+	-- 					"Targets",
+	-- 					"Microsoft.NET.Sdk.Razor.DesignTime.targets"
+	-- 				),
+	-- 			},
+	-- 			---@diagnostic disable-next-line: missing-fields
+	-- 			config = {
+	-- 				handlers = require("rzls.roslyn_handlers"),
+	-- 				settings = {
+	-- 					["csharp|inlay_hints"] = {
+	-- 						csharp_enable_inlay_hints_for_implicit_object_creation = true,
+	-- 						csharp_enable_inlay_hints_for_implicit_variable_types = true,
+	--
+	-- 						csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+	-- 						csharp_enable_inlay_hints_for_types = true,
+	-- 						dotnet_enable_inlay_hints_for_indexer_parameters = true,
+	-- 						dotnet_enable_inlay_hints_for_literal_parameters = true,
+	-- 						dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+	-- 						dotnet_enable_inlay_hints_for_other_parameters = true,
+	-- 						dotnet_enable_inlay_hints_for_parameters = true,
+	-- 						dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+	-- 						dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+	-- 						dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+	-- 					},
+	-- 					["csharp|code_lens"] = {
+	-- 						dotnet_enable_references_code_lens = true,
+	-- 					},
 	-- 				},
-	-- 				roslyn = {
-	-- 					-- When set to true, csharp.nvim will launch roslyn automatically.
-	-- 					enable = true,
-	-- 					-- Path to the roslyn LSP see 'Roslyn LSP Specific Prerequisites' above.
-	-- 					cmd_path = "Microsoft.CodeAnalysis.LanguageServer",
-	-- 				},
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- 	init = function()
+	-- 		-- we add the razor filetypes before the plugin loads
+	-- 		vim.filetype.add({
+	-- 			extension = {
+	-- 				razor = "razor",
+	-- 				cshtml = "razor",
 	-- 			},
 	-- 		})
 	-- 	end,
