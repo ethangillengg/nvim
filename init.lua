fdfaf6c2b5a23039e11a25e4bf40b959bda1d5b5
Ethan Gill <gill.ethan123@gmail.com>
2026-04-29 10:07:37 -0600

    feat: conform disable command

--- init.lua
+++ init.lua
@@ -457,9 +457,6 @@ require("lazy").setup({
 				-- -- volar = {},
 				-- -- Rust
 				-- rust_analyzer = {},
-				-- -- Python
-				-- pyright = {},
-				-- ruff = {},
 				-- C/C++
 				-- clangd = {},
 				-- glslls = {},
@@ -492,6 +489,9 @@ require("lazy").setup({
 				-- htmx = {
 				-- 	filetypes = { "html", "templ" },
 				-- },
+				-- Python
+				ruff = {},
+				basedpyright = {},
 				-- XML
 				lemminx = {},
 				lua_ls = {
@@ -535,6 +535,26 @@ require("lazy").setup({
 				desc = "[F]ormat buffer",
 			},
 		},
+		config = function(_, opts)
+			require("conform").setup(opts)
+			vim.api.nvim_create_user_command("FormatDisable", function(args)
+				if args.bang then
+					-- FormatDisable! will disable formatting just for this buffer
+					vim.b.disable_autoformat = true
+				else
+					vim.g.disable_autoformat = true
+				end
+			end, {
+				desc = "Disable autoformat-on-save",
+				bang = true,
+			})
+			vim.api.nvim_create_user_command("FormatEnable", function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+			end, {
+				desc = "Re-enable autoformat-on-save",
+			})
+		end,
 		opts = {
 			notify_on_error = false,
 			formatters = {
@@ -553,6 +573,11 @@ require("lazy").setup({
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+
 				local disable_filetypes = { c = true, cpp = true, cs = true }
 				return {
 					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
